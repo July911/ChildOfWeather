7a1e3136e9622b6dab96bbc12ccd9ba15a11044b
July911 <min95911@nate.com>
2022-04-06 23:57:24 +0900

    feat: DetailViewController에 WebView추가

--- ChildOfWeather/Presentation/DetailView/View/DetailShowUIViewController.swift
+++ ChildOfWeather/Presentation/DetailView/View/DetailShowUIViewController.swift
@@ -1,12 +1,29 @@
 import UIKit
+import WebKit
 
 final class DetailShowUIViewController: UIViewController {
     
     var viewModel: DetailShowViewModel?
 
+    private let WebView: WKWebView = {
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = preferences
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        return webView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureViewModelDelegate()
+        self.view.addSubview(WebView)
+        WebView.load(URLRequest(url: <#T##URL#>))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        WebView.frame = view.bounds
     }
     
     private func configureViewModelDelegate() {

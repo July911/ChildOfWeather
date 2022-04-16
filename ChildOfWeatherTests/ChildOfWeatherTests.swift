363b1c86e54990f24ef90420ca500f6bf942b695
July911 <min95911@nate.com>
2022-04-16 23:30:35 +0900

    test: Test bundle 생성 및 ChildOfWeather import

---
+++ ChildOfWeatherTests/ChildOfWeatherTests.swift
@@ -0,0 +1,18 @@
+import XCTest
+@testable import ChildOfWeather
+
+class ChildOfWeatherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testdone() {
+        XCTAssertEqual(0, 1)
+    }
+
+}

75e5ea7d7a8ee7349e5eed3b45d815aa6975aff8
July911 <min95911@nate.com>
2022-04-17 17:03:30 +0900

    test: CitySearchRepository 테스트 및 DTO가 Model로 변환되는지 테스트
    - 아무것도 입력받지 않았을 때 데이터를 전부 가져오는지 테스트
    - 값을 입력했을때, 정확한 값이 Search되는지 테스트
    - 변환을 담당해주는 DTO의 toDomain메서드가 정상적으로 동작하는지 테스트

--- ChildOfWeatherTests/ChildOfWeatherTests.swift
+++ ChildOfWeatherTests/ChildOfWeatherTests.swift
@@ -11,8 +11,33 @@ class ChildOfWeatherTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testdone() {
-        XCTAssertEqual(0, 1)
+    func test_CitySearchUseCase_아무것도_입력하지_않았을때_데이터를_전부_받아온다() {
+        let cityRepository = DefaultCitySearchRepository()
+        let cities = cityRepository.sortCity().count
+        let cityfromKr = cityRepository.sortCity(by: .kr).count
+        
+        XCTAssertEqual(cities, cityfromKr)
     }
-
+    
+    func test_CitySearchUseCase_yongin을_search했을때_값은_yongin이다() {
+        let cityRepository = DefaultCitySearchRepository()
+        let city = cityRepository.search(name: "yong")?.first
+        let cityName = city?.name
+        
+        XCTAssertEqual(cityName!, "Yongin")
+    }
+    
+    func test_DataLayer의_DTO인_WeatherInformation이_Model로_정상적으로_변환된다() {
+        let main = Main(temp: 32, maxTemperature: 35, minTemperature: 28)
+        let sys = Sys(sunrise: 14, sunset: 2)
+        let weather = Weather(description: "", id: 1)
+        let weatherInformation = WeatherInformation(name: "이매동", main: main, weather: [weather], sys: sys)
+        
+        let weatherFromEntity = weatherInformation.toDomain()
+        
+        XCTAssertEqual(weatherInformation.sys.sunset, weatherFromEntity.sunset)
+        XCTAssertEqual(weatherInformation.sys.sunrise, weatherFromEntity.sunrise)
+        XCTAssertEqual(weatherInformation.main.maxTemperature, weatherFromEntity.maxTemperature)
+        XCTAssertEqual(weatherInformation.main.minTemperature, weatherFromEntity.minTemperature)
+    }
 }

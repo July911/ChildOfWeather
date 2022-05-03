8075a6dfdeff76cbbeb318ee6b218050794611a8
July911 <min95911@nate.com>
2022-05-03 21:33:40 +0900

    feat: CurrentLocationViewModel 내부 구현 및 Input & Output 모델링

--- ChildOfWeather/Presentation/CurrentLocationView/ViewModel/CurrentLocationViewModel.swift
+++ ChildOfWeather/Presentation/CurrentLocationView/ViewModel/CurrentLocationViewModel.swift
@@ -1,5 +1,32 @@
 import Foundation
+import RxSwift
 
 final class CurrentLocationViewModel {
     
+    private let detailShowUseCase: DetailShowUseCase
+    private let imageCacheUseCase: ImageCacheUseCase
+    
+    init(detailShowUseCase: DetailShowUseCase, imageCacheUseCase: ImageCacheUseCase) {
+        self.detailShowUseCase = detailShowUseCase
+        self.imageCacheUseCase = imageCacheUseCase
+    }
+    
+    struct Input {
+        let viewWillAppear: Observable<Void>
+        let cachedImage: Observable<ImageCacheData>?
+        let locationChange: Observable<Bool>
+        let dismiss: Observable<Void>
+    }
+    
+    struct Output {
+        let currentImage: Observable<ImageCacheData>
+        let weatherDescription: Observable<String>
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        
+        
+        return Output(currentImage: <#T##Observable<ImageCacheData>#>, weatherDescription: <#T##Observable<String>#>)
+    }
 }
